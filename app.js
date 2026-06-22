b41df5d33026deed05c88819576158b26b74d27a
Ahmad Fachinformatiker <deutschlandahmad5@gmail.com>
2026-06-22 20:21:32 +0000

    Backend Lesson 20: GET /customers و GET /products

--- app.js
+++ app.js
@@ -4,4 +4,6 @@ const port = 3000;
 let customers = [{ id: 1, name: "Andi", city: "Dortmund" }, { id: 2, name: "Sara", city: "Berlin" }];
 let products = [{ id: 1, name: "Laptop", price: 999 }, { id: 2, name: "Mouse", price: 25 }];
 app.get("/", function (request, response) { response.status(200).json({ message: "Welcome to my API" }); });
+app.get("/customers", function (request, response) { response.status(200).json(customers); });
+app.get("/products", function (request, response) { response.status(200).json(products); });
 app.listen(port, function () { console.log("Server is running on port " + port); });
