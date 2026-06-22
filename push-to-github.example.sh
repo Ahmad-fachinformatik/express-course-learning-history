e291b06d6acc2f7fae8234e01880a490bdb09b6d
Ahmad Fachinformatiker <deutschlandahmad5@gmail.com>
2026-06-22 20:21:41 +0000

    Add GitHub push helper script

---
+++ push-to-github.example.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Replace USERNAME and REPOSITORY with your GitHub data.
+git remote add origin https://github.com/USERNAME/REPOSITORY.git
+git branch -M main
+git push -u origin main
