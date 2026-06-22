39647a8c378dc5e622bd82be27151e71c8bbcc2e
Ahmad Fachinformatiker <deutschlandahmad5@gmail.com>
2026-06-22 20:21:37 +0000

    Frontend Lesson 09: عرض عدة Customers باستخدام Array و forEach

--- frontend/script.js
+++ frontend/script.js
@@ -1,2 +1,3 @@
-const customer = { id: 1, name: "Andi", city: "Dortmund" };
-document.getElementById("customersResult").innerHTML = `<div><strong>${customer.name}</strong><br>City: ${customer.city}</div>`;
+const customers = [{ id: 1, name: "Andi", city: "Dortmund" }, { id: 2, name: "Sara", city: "Berlin" }];
+const customersResult = document.getElementById("customersResult");
+customers.forEach(function (customer) { customersResult.innerHTML += `<div><strong>${customer.name}</strong><br>City: ${customer.city}</div>`; });
