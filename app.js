fc724077001a9dbf350ea32590e74273af68ebfc
Ahmad Fachinformatiker <deutschlandahmad5@gmail.com>
2026-06-22 20:21:31 +0000

    Backend Lesson 16: فهم request و response

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const http = require("http");
 const server = http.createServer(function (request, response) {
-  response.end("Hello from Node.js server");
+  console.log(request.method);
+  console.log(request.url);
+  response.statusCode = 200;
+  response.end("Request received");
 });
 server.listen(3000, function () { console.log("Server is running on port 3000"); });
